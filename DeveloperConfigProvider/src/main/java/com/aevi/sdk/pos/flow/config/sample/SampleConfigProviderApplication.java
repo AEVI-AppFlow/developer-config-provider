dc97d41c7c809207ff854b2d50998a0361b7f4fb
Andreas Borglin <andreas@jakoon.co.uk>
2019-02-08 12:03:26 +0000

    Bumped to RC2

--- DeveloperConfigProvider/src/main/java/com/aevi/sdk/pos/flow/config/sample/SampleConfigProviderApplication.java
+++ DeveloperConfigProvider/src/main/java/com/aevi/sdk/pos/flow/config/sample/SampleConfigProviderApplication.java
@@ -1,5 +1,6 @@
 package com.aevi.sdk.pos.flow.config.sample;
 
+import com.aevi.sdk.pos.flow.PaymentApi;
 import com.aevi.sdk.pos.flow.config.BaseConfigProviderApplication;
 
 import static com.aevi.sdk.pos.flow.config.ConfigComponentProvider.getSettingsProvider;
@@ -13,8 +14,12 @@ public class SampleConfigProviderApplication extends BaseConfigProviderApplicati
     }
 
     @Override
-    protected int[] getFlowConfigs() {
-        return new int[]{R.raw.flow_sale,
+    public int[] getFlowConfigs() {
+        // TODO temporary hack to avoid sending fulfilled check to FPS 2.0.X
+        String fpsVersion = PaymentApi.getProcessingServiceVersion(this);
+        int saleFlow = fpsVersion.startsWith("2.0") ? R.raw.flow_sale : R.raw.flow_sale_with_fulfilled_check;
+
+        return new int[]{saleFlow,
                          R.raw.flow_refund,
                          R.raw.flow_reversal,
                          R.raw.flow_tokenisation,
