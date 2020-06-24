4bb409296b39f7b797fb40e90a9dc8d900410e5f
Brett Cherrington <brett@annalytics.co.uk>
2020-06-24 15:38:58 +0100

    Fix gradle.properties bug

--- writegradleprops.sh
+++ writegradleprops.sh
@@ -7,5 +7,5 @@ echo "Create gradle properties file"
 echo "org.gradle.jvmargs=-Xmx2048m -XX:MaxPermSize=512m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8" > ${OUTPUT_PROPS}
 echo "android.useAndroidX=true" >> ${OUTPUT_PROPS}
 echo "android.enableJetifier=true" >> ${OUTPUT_PROPS}
-echo "aws_accessid=$AEVI_ARTIFACTORY_AWS_ACCESS_ID" > ${OUTPUT_PROPS}
+echo "aws_accessid=$AEVI_ARTIFACTORY_AWS_ACCESS_ID" >> ${OUTPUT_PROPS}
 echo "aws_accesskey=$AEVI_ARTIFACTORY_AWS_SECRET" >> ${OUTPUT_PROPS}
